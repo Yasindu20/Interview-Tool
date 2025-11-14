82c310f0944adb2059855a1d4322a3190822fb83
Yasindu20 <yasindudemel@gmail.com>
2025-11-14 23:09:52 +0530

    auth and DB sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
+import { serve } from "inngest/express";
 
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
@@ -8,8 +10,14 @@ const app = express();
 
 const __dirname = path.resolve();
 
+// middleware
+app.use(express.json());
+app.use(cors({origin: ENV.CLIENT_URL, credentials: true}));
+
 console.log(ENV.PORT);
 console.log(ENV.DB_URL);
+
+app.use("/api/inngest", serve({ client: inngest, functions}))
 
 app.get("/health", (req, res) => {
     res.status(200).json({ msg: "api is up and running..."});
